c0b539f30725b59480963f41b6c6c8599d2823e9
ashwindev-tech <ashwini160806@gmail.com>
2025-12-18 19:42:57 +0530

    Initial Project file

--- app.py
+++ app.py
@@ -6,43 +6,34 @@ analyzer = SentimentIntensityAnalyzer()
 client = MongoClient("mongodb://localhost:27017/") 
 db = client["sentimentDB"]
 collection = db["analysis"]
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/analyze', methods=['POST'])
 def analyze():
     data = request.get_json()
     text = data.get('text', '').strip()
-
     if not text:
         sentiment = 'Neutral'
         confidence = 0.0
     else:
         scores = analyzer.polarity_scores(text)
         compound = scores['compound']
-
         if compound >= 0.05:
             sentiment = 'Positive'
         elif compound <= -0.05:
             sentiment = 'Negative'
         else:
             sentiment = 'Neutral'
-
         confidence = round(abs(compound), 2)
-
-    # Save analysis to MongoDB
     collection.insert_one({
         "text": text,
         "sentiment": sentiment,
         "confidence": confidence
     })
-
     return jsonify({
         'sentiment': sentiment,
         'confidence': confidence
     })
-
 if __name__ == '__main__':
     app.run(debug=True,use_reloader=False)
